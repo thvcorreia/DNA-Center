180978b9c085cb2a7d5ab53fe01f7fb41d2e3644
Thiago Valentim <77904384+thvcorreia@users.noreply.github.com>
2021-05-30 20:59:45 -0300

    Update 00-Install-Completo-Softlab-Programas.bat

--- 00-Install-Completo-Softlab-Programas.bat
+++ 00-Install-Completo-Softlab-Programas.bat
@@ -13,33 +13,33 @@ echo Computador: %computername%   Usuario: %username%
 @echo ==========================================================================
 pause.
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ControlLabCom.Setup.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ControlLabCom.Setup.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Atualizador de Estacao.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Atualizador de Estacao.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Autorizador de Convenios (Cliente).msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Autorizador de Convenios (Cliente).msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND B2B DBrasil.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND B2B DBrasil.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND CBHPM.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND CBHPM.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Contas Medicas (Cliente).msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Contas Medicas (Cliente).msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Documentos Atendimento.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Documentos Atendimento.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Exportador de Resultados.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Exportador de Resultados.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Exportador Softlab Cloud.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Exportador Softlab Cloud.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Integracao Chame Facil Client.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Integracao Chame Facil Client.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Integracao Fortes (Cliente).msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Integracao Fortes (Cliente).msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Laudos.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Laudos.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Qualichart.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Qualichart.msi" /qb
 
-msiexec /package "\\192.168.0.220\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Soroteca.msi" /qb
+msiexec /package "\\xxxxxxx\publico\TI\00-Programas-Instalacao-Padrao\Programas\00-softlab3\ND Soroteca.msi" /qb
 
 
 @echo ==========================================================================
@@ -52,9 +52,9 @@ mkdir C:\PWREL
 
 cacls C:\PWREL /E /T /P Todos:F
 
-msiexec /package \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\PWREL_Sem_Servico\msodbcsql_64.msi IACCEPTMSODBCSQLLICENSETERMS=YES /qb
+msiexec /package \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\PWREL_Sem_Servico\msodbcsql_64.msi IACCEPTMSODBCSQLLICENSETERMS=YES /qb
 
-msiexec /package \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\PWREL_Sem_Servico\MsSqlCmdLnUtils_64.msi IACCEPTMsSqlCmdLnUtilsLICENSETERMS=YES /qb
+msiexec /package \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\PWREL_Sem_Servico\MsSqlCmdLnUtils_64.msi IACCEPTMsSqlCmdLnUtilsLICENSETERMS=YES /qb
 
 
 @echo ==========================================================================
@@ -62,9 +62,9 @@ msiexec /package \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\PWREL
 @echo			  Verificar se ha necessidade do AC ser instalado
 @echo ==========================================================================
 
-start /w \\192.168.0.229\Fortes\AG\InstalaTerminal.exe
+start /w \\xxxxxxx\Fortes\AG\InstalaTerminal.exe
 
-start /w \\192.168.0.229\Fortes\AC\InstalaTerminal.exe
+start /w \\xxxxxxx\Fortes\AC\InstalaTerminal.exe
 
 
 @echo ==========================================================================
@@ -76,7 +76,7 @@ start /w \\192.168.0.229\Fortes\AC\InstalaTerminal.exe
 
 mkdir "C:\ProgramData\ND Engenharia e Software\S3\SOROTECA"
 
-copy \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\SOFTLAB.INI C:\Users\Public\Desktop\ /Y
+copy \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\SOFTLAB.INI C:\Users\Public\Desktop\ /Y
 
 cacls C:\Users\Public\Desktop\SOFTLAB.INI /E /P Todos:F
 
@@ -86,11 +86,11 @@ cacls "C:\ProgramData\ND Engenharia e Software" /E /T /P Todos:F
 
 cacls "C:\Program Files (x86)\ND Engenharia e Software" /E /T /P Todos:F
 
-copy \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\s3config.bin "C:\ProgramData\ND Engenharia e Software\S3\SOROTECA" /Y
+copy \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\s3config.bin "C:\ProgramData\ND Engenharia e Software\S3\SOROTECA" /Y
 
-copy \\192.168.0.229\Fortes\AG\AG.ini C:\Fortes\AG /Y
+copy \\xxxxxxx\Fortes\AG\AG.ini C:\Fortes\AG /Y
 
-copy \\192.168.0.229\Fortes\AC\AC.ini C:\Fortes\AC /Y
+copy \\xxxxxxx\Fortes\AC\AC.ini C:\Fortes\AC /Y
 
 cacls C:\Windows\SysWOW64\eventcreate.exe /E /T /P Todos:F
 
@@ -101,7 +101,7 @@ cacls C:\Windows\SysWOW64\eventcreate.exe /E /T /P Todos:F
 
 cacls C:\Windows\System32\drivers\etc\hosts /E /P Todos:F
 
-copy \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\hosts C:\Windows\System32\drivers\etc\hosts
+copy \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\hosts C:\Windows\System32\drivers\etc\hosts
 
 @echo ==========================================================================
 @echo			        Criando Atalhos do Softlab
@@ -109,7 +109,7 @@ copy \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\hos
 
 cacls C:\Users\Public\Desktop /E /P Todos:F
 
-copy \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Atalhos\*.* C:\Users\Public\Desktop\ /Y
+copy \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Atalhos\*.* C:\Users\Public\Desktop\ /Y
 
 cacls C:\Users\Public\Desktop /E /R Todos
 
@@ -122,48 +122,48 @@ cacls C:\Users\Public\Desktop /E /R Todos
 @echo			    Drivers das Impressora OKI
 @echo ==========================================================================
 
-pnputil -i -a \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\Driver-OKI4172-Win10\Driver\OKW3X055.inf
+pnputil -i -a \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\Driver-OKI4172-Win10\Driver\OKW3X055.inf
 
 
 @echo ==========================================================================
 @echo				   LibreOffice
 @echo ==========================================================================
 
-msiexec /package \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\LibreOffice\LibreOffice_7.0.4_Win_x64.msi /qb
+msiexec /package \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\LibreOffice\LibreOffice_7.0.4_Win_x64.msi /qb
 
 @echo ==========================================================================
 @echo				   Navegadores	
 @echo ==========================================================================
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\ChromeSetup.exe
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\ChromeSetup.exe
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\Firefox-Installer.exe /s
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\Firefox-Installer.exe /s
 
 @echo ==========================================================================
 @echo				     VLC
 @echo ==========================================================================
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\vlc-3.0.12-win64.exe /L=pt_BR /S
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\vlc-3.0.12-win64.exe /L=pt_BR /S
 
 @echo ==========================================================================
 @echo				 Impressora e Leitor PDF
 @echo ==========================================================================
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\CuteWriter.exe /SILENT
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\CuteWriter.exe /SILENT
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\AcroRdr_pt_BR.exe /sALL
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\AcroRdr_pt_BR.exe /sALL
 
 @echo ==========================================================================
 @echo				       7-Zip
 @echo ==========================================================================
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\7z1900-x64.exe /S
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\7z1900-x64.exe /S
 
 @echo ==========================================================================
 @echo				      Anydesk
 @echo ==========================================================================
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\AnyDesk.exe --install "C:\Program Files (x86)\Anydesk" --silent --create-shortcuts --create-desktop-icon
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\AnyDesk.exe --install "C:\Program Files (x86)\Anydesk" --silent --create-shortcuts --create-desktop-icon
 
 echo lordasus1319 | "C:\Program Files (x86)\Anydesk\AnyDesk.exe" --set-password
 
@@ -171,11 +171,11 @@ echo lordasus1319 | "C:\Program Files (x86)\Anydesk\AnyDesk.exe" --set-password
 @echo				     TeamViewer
 @echo ==========================================================================
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\TeamViewer_Setup11.exe /S
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\TeamViewer_Setup11.exe /S
 
 net stop TeamViewer
 
-regedit.exe /s \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\TeamViewer-Configuracoes.reg
+regedit.exe /s \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\TeamViewer-Configuracoes.reg
 
 net start TeamViewer
 
@@ -183,7 +183,7 @@ net start TeamViewer
 @echo				       Spark
 @echo ==========================================================================
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\spark_2_8_3.exe -q -splash
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\spark_2_8_3.exe -q -splash
 
 
 @echo ==========================================================================
@@ -193,7 +193,7 @@ start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\spa
 @echo ==========================================================================
 
 
-start /w \\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Programas\jre-8u281-windows-x64.exe /s
+start /w \\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Programas\jre-8u281-windows-x64.exe /s
 
 
 @echo ==========================================================================
