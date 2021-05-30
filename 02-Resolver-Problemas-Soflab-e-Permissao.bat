9fa2bd839c2534271ca75b2414758ab4a55b8027
Thiago Valentim <77904384+thvcorreia@users.noreply.github.com>
2021-05-30 20:53:40 -0300

    Update 02-Resolver-Problemas-Soflab-e-Permissao.bat

--- 02-Resolver-Problemas-Soflab-e-Permissao.bat
+++ 02-Resolver-Problemas-Soflab-e-Permissao.bat
@@ -1,6 +1,6 @@
 @chcp 65001
 
-copy "\\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\SOFTLAB.INI" C:\Users\Public\Desktop\
+copy "\\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\SOFTLAB.INI" C:\Users\Public\Desktop\
 
 cacls C:\Users\Public\Desktop\SOFTLAB.INI /E /P Todos:F
 
@@ -12,7 +12,7 @@ cacls "C:\ProgramData\ND Engenharia e Software" /E /T /P Todos:F
 
 cacls "C:\Program Files (x86)\ND Engenharia e Software" /E /T /P Todos:F
 
-copy "\\192.168.0.220\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\s3config.bin" "C:\ProgramData\ND Engenharia e Software\S3\SOROTECA" 
+copy "\\xxxxxxx\Publico\TI\00-Programas-Instalacao-Padrao\Configuracoes\s3config.bin" "C:\ProgramData\ND Engenharia e Software\S3\SOROTECA" 
 
 mkdir C:\PWREL
 
